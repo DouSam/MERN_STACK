61041096e1ebe906335cd2cb74ae6f2d1fbd3132
Douglas Samuel <douglassam007@gmail.com>
2022-01-20 22:26:14 -0300

    Adding comments to methods.

--- modules/trains/dalTrains.js
+++ modules/trains/dalTrains.js
@@ -5,24 +5,49 @@ const dbo = require("../../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
 module.exports = {
+  /**
+   * Find all trains documents.
+   * @returns {Array<trains>} This return a array of trains documents.
+   */
   findAll: async function () {
     let db_connect = dbo.getDb();
     return await db_connect.collection("trains").find({}).toArray();
   },
+  /**
+   * Find a unique document with `id` passed.
+   * @param {ObjectId} id This param references the document id.
+   * @returns {train} This return a unique train document.
+   */
   findById: async function (id) {
     let db_connect = dbo.getDb();
     let query = { _id: ObjectId(id) };
     return await db_connect.collection("trains").findOne(query);
   },
+  /**
+   * Create a document.
+   * @param {Object<Train>} train A train object to insert.
+   * @returns {InsertOneResult<Train>} This returns a object with the id of new document.
+   */
   create: async function (train) {
     let db_connect = dbo.getDb();
     return await db_connect.collection("trains").insertOne(train);
   },
+  /**
+   * Update a existing document.
+   * @param {ObjectId} id Id of the document that will be updated.
+   * @param {Object<Train>} newTrain A object with the fields to update.
+   * @returns {UpdateResult}
+   */
   updateById: async function (id, newTrain) {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(id) };
     return await db_connect.collection("trains").updateOne(myquery, newTrain);
   },
+  /**
+   * Delete a document.
+   * @param {ObjectId} id Id of the document that will be deleted.
+   * @returns The number of documents deleted.
+   */
   deleteById: async function (id) {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(id) };
