da36a03604a7631c1698f88456b080c4801099de
Douglas Samuel <douglassam007@gmail.com>
2022-01-20 23:41:42 -0300

    Adding routing layer to model trains.

---
+++ modules/trains/routingTrains.js
@@ -0,0 +1,35 @@
+var router = require("express").Router();
+const {
+  findAll,
+  findById,
+  create,
+  updateById,
+  deleteById,
+} = require("./serviceTrains");
+
+router.get("/", async (req, res) => {
+  let trains = await findAll();
+  res.json(trains);
+});
+
+router.get("/:id", async (req, res) => {
+  let train = await findById(req.params.id);
+  res.json(train);
+});
+
+router.post("/", async (req, res) => {
+  let result = await create(req.body);
+  res.json(result);
+});
+
+router.put("/:id", async (req, res) => {
+  let result = await updateById(req.params.id,req.body);
+  res.json(result);
+});
+
+router.delete("/:id", async (req, res) => {
+  let result = await deleteById(req.params.id);
+  res.json(result);
+});
+
+module.exports = router;
